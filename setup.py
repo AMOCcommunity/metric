4c629cf1f49d9d21662dfb2cea8c1b3c33c8cf65
Oliver Tooth <o_tooth@host596.jc.rl.ac.uk>
2024-10-07 16:21:40 +0100

    Updated package requirements and setup via pip install.

--- setup.py
+++ setup.py
@@ -22,9 +22,10 @@ CLASSIFIERS = [
     'Operating System :: OS Independent',
     'Intended Audience :: Science/Research',
     'Programming Language :: Python',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
-    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: 3.11',
+    'Programming Language :: Python :: 3.12',
     'Topic :: Scientific/Engineering',
 ]
 
@@ -36,7 +37,7 @@ setup(
     author="Fred Castruccio",
     author_email="fredc@ucar.edu",
     install_requires=install_requires,
-    python_requires='>3.5',
+    python_requires='>3.9',
     license='Apache License, Version 2.0',
     long_description=long_description,
     classifiers=CLASSIFIERS,
@@ -46,5 +47,5 @@ setup(
     zip_safe=False,
     use_scm_version={'version_scheme': 'post-release', 'local_scheme': 'dirty-tag'},
     setup_requires=['setuptools_scm', 'setuptools>=30.3.0'],
-    entry_points={'console_scripts': 'run_metric=metric.run_metric:main'},
+    entry_points={'console_scripts': 'run_metric=run_metric:main'},
 )
